ae3cf13d56d7aa7be68e89ed52ed99030b731e71
Aiyen120 <wshikder42@gmail.com>
2025-01-18 19:31:22 +0600

    Update admin.js

--- Priyansh/commands/admin.js
+++ Priyansh/commands/admin.js
@@ -27,16 +27,16 @@ var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
   
 var callback = () => api.sendMessage({body:`𝗔𝗗𝗠𝗜𝗡 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗧𝗢𝗡
 ======================
-𝗔𝗗𝗠𝗜𝗡 :ROHAN 
-𝗔𝗗𝗗𝗥𝗘𝗦𝗦:Mandari Lakshmipur Bangladesh
+𝗔𝗗𝗠𝗜𝗡 :SIAD KHAN
+𝗔𝗗𝗗𝗥𝗘𝗦𝗦:Panchagarh sadar panchagarh
 ======𝗖𝗢𝗡𝗧𝗔𝗖𝗧======
 ======================
-𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞:https://www.facebook.com/profile.php?id=100083243378088
-𝗠𝗔𝗦𝗦𝗘𝗡𝗚𝗘𝗥 𝗟𝗜𝗡𝗞:
-https://www.facebook.com/profile.php?id=100083243378088
-𝗪𝗢𝗡𝗘𝗥: ROHAN 
-𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/profile.php?id=100083243378088`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞:https://www.facebook.com/profile.php?id=100093588184623
+EMAIL:Ksiad507@gmail.com
+NO:01333867362
+𝗪𝗢𝗡𝗘𝗥: SIAD
+𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞:https://www.facebook.com/profile.php?id=100093588184623`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/100083243378088/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+      return request(encodeURI(`https://graph.facebook.com/100093588184623/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
